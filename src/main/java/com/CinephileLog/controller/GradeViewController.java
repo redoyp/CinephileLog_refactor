12e3fb4a3f7183ff0b0416b04202d8b4bf0ef3d1
gyuri0124 <gyul12243@naver.com>
2025-05-12 11:59:13 +0900

    grade description 추가

--- src/main/java/com/CinephileLog/controller/GradeViewController.java
+++ src/main/java/com/CinephileLog/controller/GradeViewController.java
@@ -24,11 +24,11 @@ public class GradeViewController {
         model.addAttribute("showMenu", true);
 
         List<Map<String, String>> gradeList = new ArrayList<>();
-        gradeList.add(createGrade("popcorn", "This is popcorn description"));
-        gradeList.add(createGrade("hotdog", "This is hotdog description"));
-        gradeList.add(createGrade("coke", "This is coke description"));
-        gradeList.add(createGrade("nachos", "This is nachos description"));
-        gradeList.add(createGrade("jelly", "This is jelly description"));
+        gradeList.add(createGrade("popcorn", " 팝콘 (리뷰 50개, 활동 지수 상위 5%) => 사이트 메인/추천탭 최상단 노출, 시사회나 이벤트 응모 우대 가능"));
+        gradeList.add(createGrade("hotdog", "핫도그 (리뷰 50개, 활동 지수 상위 10%) => 영화 컬럼 작성 가능, 작성한 리뷰 \"에디터 추천\" 뱃지 부여"));
+        gradeList.add(createGrade("nachos", "나쵸 (리뷰 30개, 좋아요 100개) => 채팅방 입장 가능"));
+        gradeList.add(createGrade("coke", "콜라 (리뷰 10개) => 나만의 영화 리스트 작성 가능"));
+        gradeList.add(createGrade("jelly", "젤리 (가입 ~ 리뷰 9개) => 리뷰 작성, 리뷰 추천 가능"));
 
         int centerX = 150 + 80;  //radius + roughly image width
         int centerY = 150 + 80;  //radius + image height
