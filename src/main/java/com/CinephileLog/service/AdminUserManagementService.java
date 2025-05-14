f4dcba35e6d3809b382f3aae2767e4a9a3bb9120
gyuri0124 <gyul12243@naver.com>
2025-05-14 10:00:40 +0900

    관리자페이지에서 포인트 수정시 마이너스 불가

--- src/main/java/com/CinephileLog/service/AdminUserManagementService.java
+++ src/main/java/com/CinephileLog/service/AdminUserManagementService.java
@@ -41,14 +41,12 @@ public class AdminUserManagementService {
 
         existingUser.setNickname(updatedUser.getNickname());
         existingUser.setGrade(updatedUser.getGrade());
-
         // 포인트 0 이상인지 확인
         if (updatedUser.getPoint() < 0) {
             throw new IllegalArgumentException("포인트는 0 이상이어야 합니다.");
         } else {
             existingUser.setPoint(updatedUser.getPoint());
         }
-
         existingUser.setIsActive(updatedUser.getIsActive());
         existingUser.setUpdatedDate(LocalDateTime.now());
         existingUser.setRole(updatedUser.getRole());
